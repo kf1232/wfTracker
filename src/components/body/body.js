5a12142a800b84526933dbe57c5309cf1e964b4b
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2021-09-12 12:53:20 -0500

    Basic Filters
    Filters page render to selection

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -6,20 +6,31 @@ import CoreLoader from './itemGroup/coreLoader'
 
 const Items = require('warframe-items')
 
+
 function Body() {
+    const [wfState, setWFState] = useState(true);
+    const [pmState, setPMState] = useState(true);
+    const [seState, setSEState] = useState(true);
+    const [meState, setMEState] = useState(true);
 
     var dataPull = new Items({ category: ['Warframes', 'Primary', 'Secondary', 'Melee'] });
-    var warframeState = useState(dataPull.filter(i => i.category === 'Warframes'));
-    var primaryState  = useState(dataPull.filter(i => i.category === 'Primary'));
-    var secondaryState = useState(dataPull.filter(i => i.category === 'Secondary'));
-    var meleState = useState(dataPull.filter(i => i.category === 'Melee'));
+    var warframeList  = useState(dataPull.filter(i => i.category === 'Warframes'));
+    var primaryList   = useState(dataPull.filter(i => i.category === 'Primary'));
+    var secondaryList = useState(dataPull.filter(i => i.category === 'Secondary'));
+    var meleeList     = useState(dataPull.filter(i => i.category === 'Melee'));
 
     return (
         <div>
-            <CoreLoader list={warframeState}/>
-            <CoreLoader list={primaryState}/>
-            <CoreLoader list={secondaryState}/>
-            <CoreLoader list={meleState}/>
+            <div>
+                <button onClick={() => setWFState(!wfState)}> Warframe {wfState} </button>
+                <button onClick={() => setPMState(!pmState)}> Primary {pmState} </button>
+                <button onClick={() => setSEState(!seState)}> Primary {seState} </button>
+                <button onClick={() => setMEState(!meState)}> Primary {meState} </button>
+            </div>
+            {wfState ? <CoreLoader list={warframeList}/> : null}
+            {pmState ? <CoreLoader list={primaryList}/> : null}
+            {seState ? <CoreLoader list={secondaryList}/> : null}
+            {meState ? <CoreLoader list={meleeList}/> : null}
         </div>
     )
 }
