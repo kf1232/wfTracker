f0ae5ba1328ff85c8a9b3c64b4c0ea87a79bff70
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2021-10-07 20:40:12 -0500

    Update item.js

--- src/components/body/item/item.js
+++ src/components/body/item/item.js
@@ -1,5 +1,5 @@
 import './item.css'
-import React, {useState} from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add, remove } from '../../../store/itemstate';
 
